0202f8c66f45b0e15cdd2f1d975a4ca06569d99d
Shad Kaske <shadkaske@gmail.com>
2021-11-29 11:40:08 -0600

    Updated cmp config

--- lua/nvim-cmp.lua
+++ lua/nvim-cmp.lua
@@ -1,4 +1,5 @@
-local cmp = require'cmp'
+local cmp = require('cmp')
+local lspkind = require('lspkind')
 
 cmp.setup({
   snippet = {
@@ -7,12 +8,21 @@ cmp.setup({
     end,
   },
 
+  formatting = {
+    format = lspkind.cmp_format({with_text = false, maxwidth = 50})
+  },
+
   sources = {
     { name = 'cmp_nvim_lsp' },
     { name = 'nvim_lua' },
     { name = 'path' },
     { name = 'luasnip' },
     { name = 'buffer', keyword_length = 5 },
+  },
+
+  experimental = {
+    native_menu = false,
+    ghost_text = true,
   },
 
 })
